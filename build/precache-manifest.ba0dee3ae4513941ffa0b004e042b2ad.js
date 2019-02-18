9796964ca4ac83d61fa8ebc77c109a6fcfa866fc
Carlan Calazans <carlancalazans@gmail.com>
2019-02-18 20:34:50 +0000

    Initial version

---
+++ build/precache-manifest.ba0dee3ae4513941ffa0b004e042b2ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "81b71a0af8cb679e54c1",
+    "url": "/static/css/main.d2c98b4b.chunk.css"
+  },
+  {
+    "revision": "81b71a0af8cb679e54c1",
+    "url": "/static/js/main.81b71a0a.chunk.js"
+  },
+  {
+    "revision": "85042e97a37d71d7ff89",
+    "url": "/static/js/1.85042e97.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ebc18432c0a71cfd876a27933f91a4a",
+    "url": "/static/media/mongoleaf.0ebc1843.png"
+  },
+  {
+    "revision": "6c93bd2010905059e08c8b522adfc219",
+    "url": "/static/media/pixelatedLeaf.6c93bd20.svg"
+  },
+  {
+    "revision": "77db89164c70583eb4c1547be66a5aa2",
+    "url": "/index.html"
+  }
+];
